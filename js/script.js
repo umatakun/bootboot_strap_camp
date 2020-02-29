b70f4a71937a183bfcad7b0b53f9d5ad5f2cb796
Yoshitsugu Koike <yoshitsugukoike@YoshitsugunoMacBook-Pro-2.local>
2020-03-01 02:33:28 +0900

    ローカル画像を表示する。

--- js/script.js
+++ js/script.js
@@ -6,4 +6,19 @@ $(function () {
     $('#datetimePicker2').datetimepicker({
         format: 'HH:mm',
     });
+    $('#profile_image').on('change', function(){
+        let fileList = this.files ;
+        let data = $(this).data();
+
+        // 個数分の画像を表示する
+        for( var i=0,l=fileList.length; l>i; i++ ) {
+            // Blob URLの作成
+            var blobUrl = window.URL.createObjectURL( fileList[i] ) ;
+
+            // HTMLに書き出し (src属性にblob URLを指定)
+            $('#' + data.srcTarget).attr('src', blobUrl);
+            break;
+        }
+
+    });
 });
