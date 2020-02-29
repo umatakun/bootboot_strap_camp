6eb77383a784a6494327a3ff2a5f2c75798893b6
Yoshitsugu Koike <yoshitsugukoike@YoshitsugunoMacBook-Pro-2.local>
2020-02-29 22:29:36 +0900

    DatetimePickerを試す。

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
-$(document).ready(function() {
+$(function () {
     $('.toast').toast({autohide: true}).toast('show');
+    $('#datetimePicker1').datetimepicker({
+        format: 'YYYY-MM-DD',
+    });
+    $('#datetimePicker2').datetimepicker({
+        format: 'HH:mm',
+    });
 });
